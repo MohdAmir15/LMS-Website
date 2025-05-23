3e0d867cf4bf6bbb8dc95a9ac3f6d8c08a713db7
Mohd Amir <you@example.commohdamir1315@gmail.com>
2025-05-23 15:04:56 +0800

    Added navbar with tailwind elements

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,12 @@ import Dashboard from './pages/educator/Dashboard';
 import AddCourse from './pages/educator/AddCourse';
 import MyCourses from './pages/educator/MyCourses';
 import StudentsEnrolled from './pages/educator/StudentsEnrolled';
+import Navbar from './components/student/Navbar.jsx';
 
 const App = () => {
   return (
-    <div>
+    <div className='text-default min-h-screen bg-white'>
+      <Navbar />
       <Routes>
 
         <Route path='/' element={<Home />} />
@@ -26,7 +28,7 @@ const App = () => {
         <Route path='/loading/:path' element={<Loading />} />
 
         <Route path='/educator' element={<Educator />}>
-          <Route path='/educator' element={<Dashboard />} />
+          <Route path='educator' element={<Dashboard />} />
           <Route path='add-course' element={<AddCourse />} />
           <Route path='my-courses' element={<MyCourses />} />
           <Route path='student-enrolled' element={<StudentsEnrolled />} />
