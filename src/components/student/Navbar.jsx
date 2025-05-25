f52b85e1a79a11b1c05c336d469ce9f65c8a9a8f
Mohd Amir <you@example.commohdamir1315@gmail.com>
2025-05-25 20:51:40 +0800

    Designed Courselist page

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         <div className='flex items-center gap-5'>
           {user &&
           <>
-            <button onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+            <button className='cursor-pointer' onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
           |<Link to='/my-enrollments' >My Enrollments</Link>
           </>
           }
@@ -35,7 +35,7 @@ const Navbar = () => {
         <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
         {user &&
           <>
-            <button onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
+            <button className='cursor-pointer' onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become Educator'}</button>
           |<Link to='/my-enrollments' >My Enrollments</Link>
           </>
           }
--- src/components/student/SearchBar.jsx
+++ src/components/student/SearchBar.jsx
@@ -17,7 +17,7 @@ const SearchBar = ({data}) => {
           <img src={assets.search_icon} alt="search_icon" className='md:w-auto w-10 px-3' />
           <input onChange={e=>setinput(e.target.value)} value={input}
           type="text" placeholder='search for courses' className='w-full h-full outline-none text-gray-500/80' />
-          <button type='submit' className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1'>Search</button>
+          <button type='submit' className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1 cursor-pointer'>Search</button>
         </form>
   )
 }
